ae6f502507453550673a780b92d4f5cef8494962
MuhamadAzmiNaziyulloh <azmi89017@gmail.com>
2024-02-29 18:02:55 +0700

    Portofolio

---
+++ home.php
@@ -0,0 +1,103 @@
+<?php include('template/header.php');?>
+
+<div class="background">
+  <img class="img-background" src="https://image.web.id/images/devhandala08521c75f6d985f.png" width="100%" height="600px" alt="">
+</div>
+<main>
+    <div class="semua-kotak">
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://image.web.id/images/icon-kursushacker.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://image.web.id/images/icon-kelasfullstack.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://www.programmerzamannow.com/img/pzn.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://image.web.id/images/Icon_CampusCoder.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+    </div>
+    <div class="semua-kotak">
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://image.web.id/images/Icon_DevHandal.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://image.web.id/images/icon-polardb.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://image.web.id/images/icon-recloud.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+        <div class="kotak">
+                <div class="img">
+                        <div class="text">
+                        
+                            <img src="https://devsummit.github.io/static/img/icon.504e1bd.png" alt="">
+                        
+                            <p class="text1"><y>KursusHacker<y><br>
+                               span Belajar Jadi Hacker Mulai Dari Nol: Latihan Aplikatif, Studi Kasus Nyata, Praktik Intensif</p>
+                    </div>
+             </div>
+        </div>
+    </div>
+</main>
+
+
+
+<?php include('template/footer.php');?>
---
+++ kategori.php
@@ -0,0 +1,58 @@
+<?php include('template/header.php');?>
+
+
+<div class="background-kategori">
+
+    <div class="kategori-semua">
+    <div class="text-kategori">
+            <h2>KURSUS CODING ZAMAN NOW</h2>
+            <h1>Kuasai Skil Coding <br>ala <span>Starup Unicorn</span></h1>
+        <p>Bangun karirmu menjadi Software Developer Expert dengan teknologi yang <br> digunakan startup teknologi dunia ditemani oleh Eko Kurniawan Khannedy<br> Technical Architect di Blibli.com.</p>
+        <button class="button-kategori">Gabung Kelas</button>
+        <button class="button2-kategori">Tanya Cs</button>
+        </div>
+        <div class="img1">
+            <img class="img-kategori" src="https://pzn.codepolitan.com/assets/img/img-hero.png" width="700px" height="500px" alt="ajaj">
+        </div>
+    </div>
+</div>
+<div class="cards">
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" style="width:600px; height:350px; margin-left:50px; display:flex;" >
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="3" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="5000">
+      <img src="https://image.web.id/images/home-banner-campuscoder.png" class="d-block " alt="..." style="width:38rem; height:21rem; border-radius:20px;">
+    </div>
+    <div class="carousel-item" data-bs-interval="1000">
+      <img src="https://image.web.id/images/home-banner-kursushacker.png" class="d-block " alt="..." style="width:38rem; height:21rem; border-radius:20px;">
+    </div>
+    <div class="carousel-item" data-bs-interval="1000">
+      <img src="https://image.web.id/images/kelas-fullstack.png" class="d-block " alt="..." style="width:38rem; height:21rem; border-radius:20px;">
+    </div>
+    <div class="carousel-item" data-bs-interval="1000">
+      <img src="https://image.web.id/images/pzn-expert.png" class="d-block " alt="..." style="width:38rem; height:21rem; border-radius:20px;">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev" style="color:white;">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+        </div>
+</div>
+
+
+
+
+
+
+
+<?php include('template/footer.php');?>
